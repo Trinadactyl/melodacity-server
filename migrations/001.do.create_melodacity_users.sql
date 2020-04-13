f0b94df3c7af08e239b1811d30cbba0ba86c96ad
Trina <trinamceary@gmail.com>
2020-04-13 16:15:17 -0700

    setup database and user table

---
+++ migrations/001.do.create_melodacity_users.sql
@@ -0,0 +1,7 @@
+CREATE TABLE "melodacity_users" (
+  id SERIAL PRIMARY KEY,
+  full_name TEXT NOT NULL,
+  user_name TEXT NOT NULL UNIQUE,
+  password TEXT NOT NULL,
+  date_created TIMESTAMPTZ NOT NULL DEFAULT now()
+);

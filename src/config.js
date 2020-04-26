d082ff16410e0909603d4fc10abac2fc280938f5
Trina <trinamceary@gmail.com>
2020-04-26 10:08:45 -0700

    deploy

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  DATABASE_URL: process.env.DB_URL || 'postgresql://trina:123@localhost/melodacity',
-  TEST_DATABASE_URL="postgresql://trina:123@localhost/melodacity-test",
+  DATABASE_URL: process.env.DATABASE_URL || 'postgresql://trina:123@localhost/melodacity',
+  TEST_DATABASE_URL:"postgresql://trina:123@localhost/melodacity-test",
   JWT_SECRET: process.env.JWT_SECRET || 'ty56gh23bnqo89'
 }

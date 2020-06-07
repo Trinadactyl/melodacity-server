79dd67eca82620c7053e507366d7af16e7291b9d
Trina <trinamceary@gmail.com>
2020-06-07 13:01:07 -0700

    auth tests

---
+++ test/auth-endpoints.spec.js
@@ -0,0 +1,91 @@
+const knex = require('knex')
+const jwt = require('jsonwebtoken')
+const app = require('../src/app')
+const helpers = require('./test-helpers')
+
+describe('Auth Endpoints', function() {
+  let db
+
+  const { testUsers } = helpers.makeFixtures()
+  const testUser = testUsers[0]
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.set('db', db)
+  })
+
+  after('disconnect from db', () => db.destroy())
+
+  before('cleanup', () => db('melodacity_users').truncate())
+
+  afterEach('Clean the table', () => db('melodacity_users').truncate())
+
+  describe(`POST /auth/login`, () => {
+    beforeEach('insert users', () => 
+      helpers.seedUsers(
+        db,
+        testUsers
+      )
+    )
+
+    const requiredFields = ['user_name', 'password']
+
+    requiredFields.forEach(field => {
+      const loginAttemptBody = {
+        user_name: testUser.user_name,
+        password: testUser.password,
+      }
+
+      it(`responds with 400 reqired error when '${field}' is missing`, () => {
+        delete loginAttemptBody[field]
+        return supertest(app)
+          .post('/auth/login')
+          .send(loginAttemptBody)
+          .expect(400, {
+            error: `Missing '${field}' in request body`,
+          })  
+      })
+    })
+
+    it(`Respods 400 'invalid user_name or password' when bad user_name`, () => {
+      const userInvalidUser = { user_name: 'user-not', password: 'existy' }
+      return supertest(app)
+      .post('/auth/login')
+      .send(userInvalidUser)
+      .expect(400, { error: `Incorrect user_name or password` })
+    })
+
+    it( `responds 4-- 'invalid user_name or password' when bad user_name`, () => {
+      const userInvalidPass = { user_name: testUser.user_name, password: 'incorrect' }
+      return supertest(app)
+      .post('/auth/login')
+      .send(userInvalidPass)
+      .expect(400, { error: `Incorect user_name or password` })
+    })
+
+    it(`responds 200 and JWT auth token using secret when valid credentials`, () => {
+      const userValidCredintials = {
+        user_name: testUser.user_name,
+        password: testUser.password,
+      }
+      const expectedToken = jwt.sign(
+        { user_id: testUser.id }, 
+        process.env.JWT_SECRET,
+        {
+          subject: testUser.user_name,
+          algorithm: 'HS256'
+        }
+      )
+      return supertest(app)
+      .post('auth/login')
+      .send(userValidCredintials)
+      .expect(200, {
+        auhtToken: expectedToken
+      })
+    })
+    
+  })
+})
--- test/user-endpoints.spec.js
+++ test/user-endpoints.spec.js
@@ -2,7 +2,7 @@ const { expect } = require('chai')
 const knex = require('knex')
 const app = require('../src/app')
 const helpers = require('./test-helpers')
-const userRouter = require('./users/user-router')
+//const userRouter = require('../../users/user-router')
 
 describe('User Endpoints', function() {
   let db
@@ -40,16 +40,29 @@ describe('User Endpoints', function() {
           testUsers,
         )
       )
-      
-
-
-
-
+      it(`responds with 200 and users`, () => {
+        return supertest(app)
+          .get('/users')
+          .expect(200, testUsers)
+      })
     })
   })
 
-
-
+  describe(`GET /users/:id`, () => {
+    context(`Given user id`, () => {
+      beforeEach('insert user', () => 
+        helpers.seedUsers(
+          db,
+          testUser
+        )
+      )
+      it(`responds 200 and the user`, () => {
+        return supertest(app)
+          .get('/user/:id')
+          .expect(200, testUser)
+      })
+    })
+  })
 
 //end of describe User Endpoints
 })
